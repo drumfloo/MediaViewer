dfc9c04b186f85c4329b3135c630476ceb173fe4
Florian Dietrich <florian.dietrich@zimmer-group.com>
2024-01-09 15:19:20 +0100

    implementing grid in zam-controller

--- projects/zam-controller/src/app/components/grid-content/grid-content.component.ts
+++ projects/zam-controller/src/app/components/grid-content/grid-content.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'app-grid-content',
   templateUrl: './grid-content.component.html',
   styleUrls: ['./grid-content.component.scss']
 })
+
+
+
 export class GridContentComponent {
+
+  public sites: any[] = [] 
+  
+
+  public ngOnInit(): void{
+    this.fetchData();
+  } 
+ 
+
+  public onCardClick(cardName: any){ 
+    window.open(cardName, '_blank');
+  }
+
+  // Not in use
+  // public deleteCard(cardName: any){
+  //   console.log(cardName + " deleted");
+  //   this.sites = this.sites.filter(value => value != cardName);
+  // }
+
+  // gets called at startup through ngOnInit()
+  // get data from database/PIM
+  public fetchData(){
+    const jsonData = require('../../data.json'); 
+
+    for(const [key, value] of Object.entries(jsonData)){
+      this.sites.push([`${key}`, `${value}`]);
+    }
+    console.log(Object.entries(jsonData));
+  }
 
 }
