d136003cd77bc1c06c5ecf01c0a5c2cb79910c48
Florian Dietrich <florian.dietrich@zimmer-group.com>
2024-01-09 10:53:51 +0100

    install videogular, implemented player

--- projects/zam-viewer/src/app/app.component.ts
+++ projects/zam-viewer/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ZAM_Viewer';
+
+  container = document.querySelector("#player")
+  
+
+  // hideMouse(){
+  //   document.body.requestPointerLock();
+  // }
+
+  // ngOnInit(){
+  //   this.hideMouse()
+  // }
 }
+
+
+
